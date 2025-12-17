332787a21aa4ff242cedbdfcbda3acc4712f8ebe
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-12-17 14:22:07 -0600

    added padding and color to dark mode;

--- src/_components/Button.tsx
+++ src/_components/Button.tsx
@@ -16,7 +16,7 @@ export default function Button({
 }: ButtonProps) {
     return (
         <button
-            className={`${className} button dark:bg-background bg-light-grey dark:text-light text-background cursor-pointer transition`}
+            className={`${className} button dark:bg-midground bg-light-grey dark:text-light text-background cursor-pointer transition md:p-3 md:text-2xl`}
             onClick={(e) => {
                 if (preventDefault) {
                     e.preventDefault()
